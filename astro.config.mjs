6aa16b544a3cc328bad3bdf86193f22cb4b6ab68
Alex Veenendaal <alex@opendoorgonorth.com>
2025-05-20 12:05:19 +0100

    chore: rss/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap()],
 
   markdown: {
     shikiConfig: {
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,16 @@
-import { getCollection } from 'astro:content';
-
 import rss from '@astrojs/rss';
+import { getPublishedContent } from '@helpers/posts';
 
 import { SITE_DESCRIPTION, SITE_TITLE } from '../constants';
 
 export const GET = async context => {
-  const posts = await getCollection('posts');
+  const posts = await getPublishedContent(true);
+
   return rss({
     description: SITE_DESCRIPTION,
     items: posts.map(post => ({
       ...post.data,
-      link: `/posts/${post.id}/`
+      link: post.url
     })),
     site: context.site,
     title: SITE_TITLE
